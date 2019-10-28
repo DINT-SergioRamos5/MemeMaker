61961621982665d39d460645c2f17e9421eebaf4
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-10-28 12:18:47 +0100

    Terminado

--- MemeMaker/MainWindow.xaml.cs
+++ MemeMaker/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,27 @@ namespace MemeMaker
         public MainWindow()
         {
             InitializeComponent();
+        }
+    }
+
+    public class BooleanCheckBox : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Border b = new Border();
+            if ((bool)value)
+            {
+                return b.BorderThickness = new Thickness(5);
+            }
+            else
+            {
+                return b.BorderThickness = new Thickness(0);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
